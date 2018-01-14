0ec1009cd3ce7016c0201ad8c0a4dd9b3e0c063e
BuildTools <unconfigured@null.spigotmc.org>
2018-01-14 10:26:26 -0500

    Update

--- CheatEngine/Utilities.py
+++ CheatEngine/Utilities.py
@@ -1,4 +1,44 @@
 import battlecode as bc
+import heapq
+
+
+def get_direction(loc1, loc2):
+
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+
+    def empty(self):
+        return len(self.elements) == 0
+
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+
+def neighbors(current, gamemap, gc):
+    nearby = []
+    for i in range(current.x - 1, current.x + 1):
+        for j in range(current.y - 1, current.y -1):
+            if not i == current.x and not j == current.y and gamemap.on_map(bc.MapLocation(gc.planet(), i, j)):
+                nearby.append(bc.MapLocation(gc.planet, i, j))
+    return nearby
+
+
+def cost(loc, game_map):
+    if not game_map.is_passable_terrain_at(loc):
+        return 10000
+    return 1
+
+
+def heuristic(node, goal):
+    p = 1.001
+    dx = abs(node.x - goal.x)
+    dy = abs(node.y - goal.y)
+    return (dx + dy - 1 * min(dx, dy)) * p
 
 
 class Utilities:
@@ -17,5 +57,33 @@ class Utilities:
         gc
 
     @staticmethod
-    def find_path(loc1, loc2, game_map):
-        loc1
+    def find_path(loc1, loc2, game_map, gc):
+        frontier = PriorityQueue()
+        frontier.put(loc1,0)
+        came_from = {}
+        cost_so_far = {}
+        came_from[loc1] = None
+        cost_so_far[loc1] = 0
+
+        while not frontier.empty():
+            current = frontier.get()
+
+            if current == loc2 :
+                break
+            for next in neighbors(current,game_map,gc):
+                new_cost = cost_so_far[current] + cost(next)
+                if next not in cost_so_far or new_cost < cost_so_far[next]:
+                    cost_so_far[next] = new_cost
+                    priority = new_cost + heuristic(loc2, next)
+                    frontier.put(next, priority)
+                    came_from[next] = current
+        return came_from, cost_so_far
+
+    @staticmethod
+    def nearest_type(unit, type, distance):
+        nearest_dist = 10000000
+        for i in range(unit.location.map_location().x - distance, unit.location.map_location().x + distance):
+            for j in range(unit.location.map_location().y - distance, unit.location.map_location().y + distance):
+
+
+
--- CheatEngine/run.py
+++ CheatEngine/run.py
@@ -14,15 +14,9 @@ from Worker import Worker as worker
 
 # Import dependencies end
 
-# Obtain game values to reduce module calls
-# A GameController is the main type that you talk to the game with.
-# Its constructor will connect to a running game.
-gc = bc.GameController()
-# orbit_pattern = bc.OrbitPattern()
-directions = list(bc.Direction)
 
 print("pystarted")
-# print(str(orbit_pattern))
+
 # It's a good idea to try to keep your bots deterministic, to make debugging easier.
 # determinism isn't required, but it means that the same things will happen in every thing you run,
 # aside from turns taking slightly different amounts of time due to noise.
@@ -31,14 +25,32 @@ random.seed(6137)
 
 # Obtain value end
 
+#Define Variables that we will call within the method
+# Obtain game values to reduce module calls
+# A GameController is the main type that you talk to the game with.
+# Its constructor will connect to a running game.
+gc = bc.GameController()
+gm = bc.GameMap
+# orbit_pattern = bc.OrbitPattern()
+directions = list(bc.Direction)
+my_team = gc.team()
+earth_map = gm.earth_map
+mars_map = gm.mars_map
+asteroid_pattern = gm.asteroids
+
+# Define variables end
+
 # Define Functions that we will call within the method
+
+
 def resolve_unit(unit):
+    path = []
     if unit.unit_type == bc.UnitType.Factory:
         factory.factory(unit, gc)
     if unit.unit_type == bc.UnitType.Worker:
-        worker.worker(unit, gc)
-        utilities.move(unit, gc, random.choice(directions))
-
+        path = worker.worker(unit, gc, earth_map, mars_map, [])
+    if not unit.unit_type == bc.UnitType.Factory and not unit.unit_type == bc.UnitType.Rocket:
+        utilities.move(unit, gc, path)
 # let's start off with some research!
 # we can queue as much as we want.
 
@@ -47,7 +59,7 @@ gc.queue_research(bc.UnitType.Rocket)
 gc.queue_research(bc.UnitType.Worker)
 gc.queue_research(bc.UnitType.Knight)
 
-my_team = gc.team()
+
 while True:
     # We only support Python 3, which means brackets around print()
     print('pyround:', gc.round())
